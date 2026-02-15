defa5afa17efad644bf5402a3990ef7e257827cb
Loay Al-Said <92930196+loayalsaid1@users.noreply.github.com>
2026-02-15 22:01:37 +0000

    Add basic instructures file and sort tailwind in teh Page.tsx

---
+++ .github/instructions.md
@@ -0,0 +1,1 @@
+All Generated Code must me clean code & well modulraized. For Front-end, for example, reusable components, hooks, functions, services, etc.
--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,9 +1,13 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex items-center justify-center w-full min-h-screen font-sans bg-zinc-50 dark:bg-black">
+      <header>
+        <SidebarTrigger />
+      </header>
+      <main className="flex flex-col items-center justify-between w-full max-w-3xl min-h-screen px-16 py-32 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
