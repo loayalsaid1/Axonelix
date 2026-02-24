6fcb330035cf0584148672f67e379494f713f59b
Loay Al-Said <l.programming555@gmail.com>
2026-02-24 13:34:15 +0000

    Merge feature/themes ... Squashed commit of the following:
    commit 713922a87b3df89df3d8f4a8b7b5daf684e70be1
    Author: Loay Al-Said <l.programming555@gmail.com>
    Date:   Tue Feb 24 13:14:48 2026 +0000
    
        Implement theme provider and switcher components; add color theme context and configuration
    
    commit 4c625cfdd8422f07783b37dcc11c8ef2996161e0
    Author: Loay Al-Said <l.programming555@gmail.com>
    Date:   Tue Feb 24 13:14:32 2026 +0000
    
        Add the themes css files
    
    commit 2ebe6bd4512e349b8adbe6163ed3997d61f20d67
    Author: Loay Al-Said <l.programming555@gmail.com>
    Date:   Tue Feb 24 12:20:31 2026 +0000
    
        Edit base CSS variables to the slate theme from shadcn

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 import { shadcn } from '@clerk/themes'
+import { AppThemeProvider, ColorThemeScript } from "@/components/theme-provider"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,11 +29,16 @@ export default function RootLayout({
     <ClerkProvider appearance={{
       theme: shadcn
     }}>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <ColorThemeScript />
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <AppThemeProvider>
+            {children}
+          </AppThemeProvider>
         </body>
       </html>
     </ClerkProvider>
