05f4b2eaebd4083be7f5846e7e18893111df9718
Loay Al-Said <l.programming555@gmail.com>
2026-02-24 05:55:55 +0000

    Create the proxy.ts to setup clerk and include the provider in the app layout

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
+import { ClerkProvider } from '@clerk/nextjs'
+import { shadcn } from '@clerk/themes'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,19 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <SidebarProvider>
-          <TooltipProvider>
-            <AppSidebar/>
-            <SidebarInset>
-              {children}
-            </SidebarInset>
-          </TooltipProvider>
-        </SidebarProvider>
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      theme: shadcn
+    }}>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
