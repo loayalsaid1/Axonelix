5bf60626fe9c40fd71b67facf2b32633676a746d
Loay Al-Said <92930196+loayalsaid1@users.noreply.github.com>
2026-03-09 04:19:08 +0000

    feat: add bulk create questions functionality with DTOs and service method

--- Back-End/src/modules/questions/questions/dto/index.ts
+++ Back-End/src/modules/questions/questions/dto/index.ts
@@ -3,3 +3,4 @@ export { UpdateQuestionDto } from './update-question.dto';
 export { QuestionResponseDto, QuestionOptionResponseDto } from './question-response.dto';
 export { QuestionFilterDto } from './question-filter.dto';
 export { PaginatedQuestionsDto, QuestionIdsDto } from './paginated-questions.dto';
+export { BulkCreateQuestionsDto, BulkCreateResultDto } from './bulk-create-question.dto';
