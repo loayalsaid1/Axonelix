80199f7b26e244500440941fe868e40d08ccf315
loay al-said <l.programming555@gmail.com>
2026-03-09 14:38:33 +0200

    Add onDelete Set Null to the old-exams foreign keys

--- Back-End/src/database/entities/old-exams.ts
+++ Back-End/src/database/entities/old-exams.ts
@@ -14,15 +14,15 @@ export const oldExams = pgTable("old_exams", {
 }, (table) => [
 	index("idx_old_exams_lookup").using("btree", table.universityId.asc().nullsLast().op("int4_ops"), table.year.asc().nullsLast().op("int4_ops"), table.examType.asc().nullsLast().op("text_ops"), table.moduleId.asc().nullsLast().op("int4_ops")),
 	foreignKey({
-			columns: [table.moduleId],
-			foreignColumns: [modules.id],
-			name: "old_exams_module_id_fkey"
-		}),
+		columns: [table.moduleId],
+		foreignColumns: [modules.id],
+		name: "old_exams_module_id_fkey"
+	}).onDelete('set null'),
 	foreignKey({
-			columns: [table.universityId],
-			foreignColumns: [universities.id],
-			name: "old_exams_university_id_fkey"
-		}),
+		columns: [table.universityId],
+		foreignColumns: [universities.id],
+		name: "old_exams_university_id_fkey"
+	}).onDelete('set null'),
 	unique("old_exams_exam_type_module_id_module_type_university_id_yea_key").on(table.examType, table.moduleId, table.moduleType, table.universityId, table.year),
 	check("old_exams_exam_type_check", sql`(exam_type)::text = ANY ((ARRAY['final'::character varying, 'midterm'::character varying, 'tpl'::character varying, 'flipped'::character varying])::text[])`),
 	check("old_exams_module_type_check", sql`(module_type)::text = ANY ((ARRAY['theoretical'::character varying, 'practical'::character varying])::text[])`),
