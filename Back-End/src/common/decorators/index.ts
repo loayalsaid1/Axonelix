41d0662acd13076c817546eff009d8d759177eb8
Loay Al-Said <92930196+loayalsaid1@users.noreply.github.com>
2026-03-10 04:09:39 +0000

    Add IsPublic decorator to mark routes as publicly accessible

--- Back-End/src/common/decorators/index.ts
+++ Back-End/src/common/decorators/index.ts
@@ -1,3 +1,4 @@
 export { paramIntId } from './param-int-id.decorator';
 export { CurrentUser } from './current-user.decorator';
 export { Roles } from './roles.decorator';
+export { IsPublic } from './is-public.decorator';
