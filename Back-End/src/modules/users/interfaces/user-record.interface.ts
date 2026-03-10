a7d9b1c5ce0a33049bbabaca5f751db2164c1f12
Loay Al-Said <92930196+loayalsaid1@users.noreply.github.com>
2026-03-10 00:45:25 +0000

    Update UserRecord interface to use Role enum for user role

--- Back-End/src/modules/users/interfaces/user-record.interface.ts
+++ Back-End/src/modules/users/interfaces/user-record.interface.ts
@@ -1,9 +1,11 @@
+import { Role } from '../../../common/enums';
+
 /** Shape of a user row returned from the database. */
 export interface UserRecord {
   id: number;
   clerkId: string;
   email: string;
-  role: string;
+  role: Role;
   createdAt: string | null;
   updatedAt: string | null;
 }
