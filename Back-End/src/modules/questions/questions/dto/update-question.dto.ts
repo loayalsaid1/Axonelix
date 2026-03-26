489e9ee5f443237b4ae39f0f7ad1a110299a3efa
Loay Al-Said <l.programming555@gmail.com>
2026-03-26 04:19:18 +0000

    feat: extend UpdateQuestionDto with additional fields for lessonId, chapterId, isMisc, and oldExamId

--- Back-End/src/modules/questions/questions/dto/update-question.dto.ts
+++ Back-End/src/modules/questions/questions/dto/update-question.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateQuestionDto } from './create-question.dto';
+import { IsNumber, IsBoolean, ValidateIf } from 'class-validator';
 
-export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {}
+export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {
+	@IsNumber()
+	@ValidateIf((_, value) => value !== null && value !== undefined)
+	lessonId?: number | null;
+
+	@IsNumber()
+	@ValidateIf((_, value) => value !== null && value !== undefined)
+	chapterId?: number | null;
+
+	@IsBoolean()
+	@ValidateIf((_, value) => value !== null && value !== undefined)
+	isMisc?: boolean | null;
+
+	@IsNumber()
+	@ValidateIf((_, value) => value !== null && value !== undefined)
+	oldExamId?: number | null;
+}
