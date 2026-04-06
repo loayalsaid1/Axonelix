e6d75add739490d32f5b5f04358cfd2601646f09
Loay Al-Said <l.programming555@gmail.com>
2026-04-06 06:01:07 +0000

    feat: add lesson and hierarchy types for flashcards functionality

---
+++ Frontend/lib/types/lesson-hierarchy.types.ts
@@ -0,0 +1,36 @@
+export interface ModuleOption {
+	id: number;
+	name: string;
+}
+
+export interface SubjectOption {
+	id: number;
+	name: string;
+	type: string;
+}
+
+export interface ChapterOption {
+	id: number;
+	name: string;
+}
+
+export interface LessonOption {
+	id: number;
+	name: string;
+}
+
+export interface SubjectResponse {
+	id: number;
+	name: string;
+	type: string;
+}
+
+export interface ChapterResponse {
+	id: number;
+	name: string;
+}
+
+export interface LessonResponse {
+	id: number;
+	name: string;
+}
