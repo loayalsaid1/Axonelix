cc4d8c11b53b780f1202197849e4e2eb5ca3ae04
Loay Al-Said <l.programming555@gmail.com>
2026-04-08 14:19:45 +0000

    Add a basic manifest file

---
+++ Frontend/app/manifest.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from "next";
+import Logo from '@/public/images/logo.png';
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		name: "Axonelix Medical Hub",
+		short_name: "Axonelix",
+		start_url: "/",
+		display: "standalone",
+		background_color: "#ffffff",
+		theme_color: "#000000",
+		icons: [
+			{
+				src: Logo.src,
+				type: "image/png"
+			}
+		]
+	};
+}
